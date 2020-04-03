ba4113a1647054e6afcb502101c035409917a3dd
byl0561 <byl0561@gmail.com>
2020-04-03 16:40:44 +0800

    reconstruct for muti-lang

--- java/Config.java
+++ java/Config.java
@@ -1,5 +1,5 @@
 public interface Config {
-    String DATA_PATH = "data/test_data.txt";
+    String DATA_PATH = "java/test_data.txt";
     int V_COUNT = 55000;
     int E_COUNT = 280000;
     int MAX_MONEY = 10000;
